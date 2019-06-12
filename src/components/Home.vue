1dc1f2a656412605a2b7bda77e399b102d7039fb
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-06-12 16:14:57 -0700

    Practice basic

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+       {{ status  }}
+   </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            status: 'Critical'
+        }
+    }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
