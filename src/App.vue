d5b8be8ea5e82562a0d1fada24aea143c4d3d1c2
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-06-17 21:07:15 -0700

    Save button animation

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,45 @@
 <template>
   <div>
-      <div>
-        <h1>{{ title }}</h1>
-      </div>
-      <hr>
-      <h2>{{ title | toUpperCase}}</h2>
-      <hr>
-      <label for="fruit">Fruit :</label>
-      <input type="text" name="fruit" id="fruit" v-model="input">
-      
-      <ul>
-        <li v-for="fruit in filteredFruits" v-bind:key="fruit">{{fruit}}</li>
-      </ul>
-      <hr>
-      <app-list></app-list>
-  </div>
-  
+    <h1>{{ happy }} </h1>
+    <button @click="happy = !happy">Show/Hide</button>
+    <br>
+    <transition name="fade">
+      <p class="para" v-if="happy">
+        Happiness is what I want
+      </p>
+    </transition>
+    
+  </div>  
 </template>
 
 <script>
-  import List from './components/List.vue'
-  export default {
-    data: function() {
-      return {
-        title: 'Hello There',
-        fruits: ['Apple', 'Banana', 'Pear', 'Melon'],
-        input: ''
-      }
-    },
-    filters: {
-      toUpperCase: (text) => {
-        return text.toUpperCase()
-      }
-    },
-    computed: {
-      filteredFruits() {
-        return this.fruits.filter( (fruit) => {
-          return fruit.match(this.input)  
-        })
-      }
-    },
-    components: {
-      appList: List
+export default {
+  data: function() {
+    return {
+      happy: true
     }
   }
+}
 </script>
 
 <style>
+  .para {
+    background-color: red; 
+  }
+  .fade-enter {
+    opacity: 0;
+  }
 
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0
+  }
 </style>
