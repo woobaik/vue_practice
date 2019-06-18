82dbc609ebe209ca72f5eaa3b28832792e9a626b
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-06-17 21:20:38 -0700

    Initialize the button app

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,33 @@
 <template>
   <div>
-    <h1>{{ happy }} </h1>
-    <button @click="happy = !happy">Show/Hide</button>
-    <br>
-    <transition name="fade">
-      <p class="para" v-if="happy">
-        Happiness is what I want
-      </p>
-    </transition>
-    
-  </div>  
+    <h1>Practice Name :: {{ title }}</h1>  
+
+    <hr>
+    <button @click="show=!show">{{ button }}</button>
+     value check [ {{ show }} ]
+
+     <p v-if="show" class="sample">
+       This is a sample test
+     </p>
+  </div>
+  
 </template>
 
 <script>
 export default {
   data: function() {
     return {
-      happy: true
+      title: 'Animation',
+      button: 'Show / Hide',
+      show: true
     }
   }
 }
 </script>
 
 <style>
-  .para {
-    background-color: red; 
-  }
-  .fade-enter {
-    opacity: 0;
-  }
-
-  .fade-enter-active {
-    transition: opacity 1s;
-  }
-
-  .fade-leave {
-    
-  }
-
-  .fade-leave-active {
-    transition: opacity 1s;
-    opacity: 0
+  .sample {
+    background-color: lightcoral;
+    border: 4px solid black;
   }
 </style>
